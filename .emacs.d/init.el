5c3724737209d5378300f5426c69e70f85aa1298
niku <niku@niku.name>
2015-04-25 12:11:43 +0900

    Add `helm-migemo` for searching Japanese with alphabet in helm

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -183,6 +183,23 @@
    ("M-%" . helm-regexp)
    ("<help> a" . helm-apropos)))
 
+(el-get-bundle helm-migemo)
+(use-package helm-migemo
+  :config
+  ;; Patch from http://rubikitch.com/2014/12/19/helm-migemo/
+  (eval-after-load "helm-migemo"
+    '(defun helm-compile-source--candidates-in-buffer (source)
+       (helm-aif (assoc 'candidates-in-buffer source)
+           (append source
+                   `((candidates
+                      . ,(or (cdr it)
+                             (lambda ()
+                               ;; Do not use `source' because other plugins
+                               ;; (such as helm-migemo) may change it
+                               (helm-candidates-in-buffer (helm-get-current-source)))))
+                     (volatile) (match identity)))
+         source))))
+
 ;; git client
 (el-get-bundle magit)
 (use-package magit
