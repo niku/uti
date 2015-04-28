b9ac7a728a1d29e92e5d61c602a085a9e43667e2
niku <niku@niku.name>
2015-04-29 05:46:18 +0900

    Adjust for local building environment

--- share/lib/server/host/scripts/vmtools.sh
+++ share/lib/server/host/scripts/vmtools.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+apt-get install -y build-essential module-assistant
+echo 'yes' |  m-a prepare
 mkdir /tmp/vbox
-VER=$(cat /home/vagrant/.vbox_version)
-mount -o loop /home/vagrant/VBoxGuestAdditions_$VER.iso /tmp/vbox
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/vbox
 sh /tmp/vbox/VBoxLinuxAdditions.run
 umount /tmp/vbox
 rmdir /tmp/vbox
