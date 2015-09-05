a25544d78439a0f800594ee78cba3a0c37d3f5e8
niku <niku@niku.name>
2015-09-06 02:38:23 +0900

    Enable history in erl/iex
    https://github.com/ferd/erlang-history

--- share/bin/machine-setup.sh
+++ share/bin/machine-setup.sh
@@ -127,3 +127,10 @@ brew cask cleanup
 
 ln -s ${ROOT_DIR}/.emacs.d ~/
 ln -s ${ROOT_DIR}/share ~/
+
+# shell history for erl / iex
+# https://github.com/ferd/erlang-history
+cd /var/tmp && \
+    git clone ferd/erlang-history && \
+    cd erlang-history && \
+    sudo make install
