eb141593e1883505096c1ede7a6cbc29f9d31b5e
niku <niku@niku.name>
2015-09-06 01:02:18 +0900

    Remove the patch and helm-migemo
    This patch breaks behavior of helm.
    And helm-migemo doesn't make sense if helm doesn't work well.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -221,23 +221,6 @@
   ;; 同期的に読み込まれるようにする
   :demand t)
 
-(el-get-bundle helm-migemo)
-(use-package helm-migemo
-  :config
-  ;; Patch from http://rubikitch.com/2014/12/19/helm-migemo/
-  (eval-after-load "helm-migemo"
-    '(defun helm-compile-source--candidates-in-buffer (source)
-       (helm-aif (assoc 'candidates-in-buffer source)
-           (append source
-                   `((candidates
-                      . ,(or (cdr it)
-                             (lambda ()
-                               ;; Do not use `source' because other plugins
-                               ;; (such as helm-migemo) may change it
-                               (helm-candidates-in-buffer (helm-get-current-source)))))
-                     (volatile) (match identity)))
-         source))))
-
 ;; インクリメンタルサーチを機能拡張する
 (el-get-bundle ace-isearch
   :depends (avy helm-swoop ace-jump-mode))
