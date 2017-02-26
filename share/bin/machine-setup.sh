582e2f705813d81e6166299c7ae163548eed4f0b
niku <niku@niku.name>
2017-02-26 22:31:21 +0900

    Use Focus.app

--- share/bin/machine-setup.sh
+++ share/bin/machine-setup.sh
@@ -76,6 +76,7 @@ brew cask install licecap
 brew cask install docker
 brew cask install iterm2
 brew cask install intellij-idea
+brew cask install focus
 brew cask cleanup
 
 brew tap homebrew/binary
